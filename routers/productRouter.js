57cc2a8f39ab543890fb6119c1fd73c271544c8a
codevitax <122753201+codevitax@users.noreply.github.com>
2023-01-24 18:05:32 -0400

    adding router

--- routers/productRouter.js
+++ routers/productRouter.js
@@ -3,18 +3,42 @@ const productRouter = express.Router();// esta funcion es para crear un router e
 
 productRouter
     .route("/")
-    .get((req, res, next) => {
+    .get((req, res) => {
         res.status(200).json({
             status: "ok",
             action: "get all products"
         });
     })
-    .post((req, res, next) => {
+    .post((req, res) => {
         console.log(req.body);
         res.status(200).json({
             status: "ok",
-            action: "post all products"
+            action: "post all products",
+            params: req.body
         });
     });
+productRouter
+    .route("/:id")
+    .get((req, res) => {
+        res.status(200).json({
+            status: "ok",
+            actio: "get a product",
+            id: req.params.id
+        })
+    })
+    .put((req, res) => {
+        res.status(200).json({
+            status: "ok",
+            action: "modify a product",
+            id: req.params.id
+        })
+    })
+    .delete((req, res) => {
+        res.status(200).json({
+            status: "ok",
+            action: "delete a product",
+            id: req.params.id
+        })
+    })
 
 module.exports = productRouter;
