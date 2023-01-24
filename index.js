4b2956a28fbf13e6f778ea3500e859ab59fe6f29
codevitax <122753201+codevitax@users.noreply.github.com>
2023-01-24 12:45:36 -0400

    middlewares

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.use((req, res, next) => {
+    //guardar en los logs la peticion
+    next();
+});
 
 app.get("/api/test", (req, res) => {
+    console.log(req.body);
     res.status(200).json({
         peticion: "GET",
         ruta: "/api/test"
     });
 });
 
+app.get("/api/test2", (req, res) => {
+    console.log(req.body);
+    res.status(200).json({
+        peticion: "GET",
+        ruta: "/api/test"
+    });
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`App running on port ${process.env.PORT}`);
