2f0c586e8df3e1c2d2b4bd4a4d44421c3b6836eb
codevitax <122753201+codevitax@users.noreply.github.com>
2023-01-25 12:53:05 -0400

    connect to mongo

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 const productRouter = require("./routers/productRouter");
 const app = express();
 app.use(express.json());// req => body
@@ -15,3 +16,10 @@ app.use("/api/v1/product/", productRouter);
 app.listen(process.env.PORT, () => {
     console.log(`App running on port ${process.env.PORT}`);
 });
+
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.DB_URL, {}).then((con) => {
+    console.log("connected to mongo");
+}).catch((err) => {
+    console.log("error");
+})
