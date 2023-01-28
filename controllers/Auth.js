04b1a861cfbcbadc7cee05c12d2938fa6408d38a
codevitax <122753201+codevitax@users.noreply.github.com>
2023-01-28 12:13:07 -0400

    jwt

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,14 +1,36 @@
-const Product = require("../models/User");
-const catchAsync = require("../utils/catchAsync")
-
+const User = require("../models/User");
+const catchAsync = require("../utils/catchAsync");
+const jwt = require("jsonwebtoken");
 
 const login = catchAsync(async (req, res) => {
-    const products = await Product.findOne();
+    let { email, password } = req.body;
+    if(!email || !password) {
+        throw new Error("Please provide email and password")
+    }
+
+    const user = await User.findOne({ email }) // es equivalnete a email:email
+
+    if(!user || user.password !== password) {
+        throw new Error("Invalid email or password");
+    }
+
+    const token = jwt.sign({ id: user._id, email, firstName: user.firstName, lastName: user.lastName }, process.env.JWT_SECRET, 
+        { expiresIn: process.env.JWT_EXPIRES_IN }
+    );
+
+    const cookieOptions = {
+        expires: new Date( Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 10000),
+        httpOnly: true
+    }
+
+    res.cookie("jwt", token, cookieOptions)
+
     res.status(200).json({
         status: "ok",
-        message: "estas intentando loguearte",
+        token
     });
 });
+
 
 module.exports = {
     login
